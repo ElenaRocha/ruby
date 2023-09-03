990c0b4acdd5c7dbd0129f2c58fa0eaf828f4b0e
Elena Rocha <ele_r.v@hotmail.com>
2023-09-03 20:38:10 +0200

    segundo ejercicio de Ruby

--- hello_world.rb
+++ hello_world.rb
@@ -1,6 +1,41 @@
-#puts "Hello world"
+#puts "Hello World"
 #print "hello world"
-print "¿Cómo te llamas?"
+print "¿What's your name?"
 name = gets
 #print name
-print "Hola #{name}"
+print "Hi #{name}"
+
+
+#ejercicio chatbot
+puts "Hi, my name is Ruby!"
+puts "What is your name?"
+name = gets.chomp
+puts "Hi there #{name}, great to meet you!"
+
+while (question = gets.chomp) != 'Bye'
+  if question == 'How old are you?'
+    puts "I'm 20 years old"
+  end
+
+  if question == 'Where do you live?'
+    puts "Japan"
+  end
+end
+
+puts "Thanks for chatting!"
+
+#ejercicio chatbot 2
+puts "Hi, my name is Ruby!"
+puts "What is your name?"
+name = gets.chomp
+puts "Hi there #{name}, great to meet you!"
+
+if question == 'How old are you?'
+  puts "I'm 20 years old"
+end
+
+if question == 'Where do you live?'
+  puts "Japan"
+end
+
+puts "Thanks for chatting!"
