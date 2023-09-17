66b0d3233637cb2c3afa1ba8ec2c32949f1e1f8e
Elena Rocha <ele_r.v@hotmail.com>
2023-09-17 21:57:50 +0200

    sigo practicando ruby

--- hello_world.rb
+++ hello_world.rb
@@ -218,4 +218,33 @@
 
 
 
+# #práctica de clases
+# attr_accessor :name, :email, :age
+
+# def initialize(name, email, age)
+#   self.name = name
+#   self.email = email
+#   self.age = age
+# end
+
+# def celebrate_birthday
+#   puts "Happy Birthday!"
+#   self.age += 1
+# end
+
+# def to_s
+#   "#{self.name}"
+# end
+
+# def inspect
+#   "[#{name}, #{email}, #{age}]"
+# end
+# end
+
+# user = User.new('Elvis', 'elvis@presley.com', 55)
+# puts user
+# p user
+
+
+
 #práctica
