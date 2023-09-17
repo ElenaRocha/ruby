0ef846aeab1021ef5afbe1ff86a1551acd47d1e4
Elena Rocha <ele_r.v@hotmail.com>
2023-09-17 22:02:13 +0200

    ejercicio clases corregido

--- hello_world.rb
+++ hello_world.rb
@@ -218,33 +218,30 @@
 
 
 
-# #práctica de clases
-# attr_accessor :name, :email, :age
+#práctica de clases
+# class User
+#   attr_accessor :name, :email, :age
 
-# def initialize(name, email, age)
-#   self.name = name
-#   self.email = email
-#   self.age = age
-# end
+#   def initialize(name, email, age)
+#     self.name = name
+#     self.email = email
+#     self.age = age
+#   end
 
-# def celebrate_birthday
-#   puts "Happy Birthday!"
-#   self.age += 1
-# end
+#   def celebrate_birthday
+#     puts "Happy Birthday!"
+#     self.age += 1
+#   end
 
-# def to_s
-#   "#{self.name}"
-# end
+#   def to_s
+#     self.name
+#   end
 
-# def inspect
-#   "[#{name}, #{email}, #{age}]"
-# end
+#   def inspect
+#     "[#{self.name}, #{self.email}, #{self.age}]"
+#   end
 # end
 
 # user = User.new('Elvis', 'elvis@presley.com', 55)
 # puts user
 # p user
-
-
-
-#práctica
