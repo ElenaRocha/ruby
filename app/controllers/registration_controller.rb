fb402aef0c3d75f45c5eb4cf86f409622ab95067
Elena Rocha <elena.rocha@globaleduca.com>
2023-09-26 13:59:38 +0200

    .

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,4 +2,21 @@ class RegistrationController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        #params: Parameters => {"authenticity_token"=>"[FILTERED]", "user"=>{"email"=>"elena@gmail.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign up"}
+        #render plain: params[:user] => {"email"=>"elena@gmail.com", "password"=>"+3>.Map4t$Q4V%H", "password_confirmation"=>"+3>.Map4t$Q4V%H"}
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
